684dc7ffe9af38975a605e4b0c19381f8bb8fe43
Taylor Lineman <taylor.lineman@gmail.com>
2024-04-08 17:28:40 -0400

    Added new defines

--- Package.swift
+++ Package.swift
@@ -43,12 +43,9 @@ let package = Package(
             publicHeadersPath: "spm-headers",
             cSettings: [
                 .unsafeFlags(["-Wno-shorten-64-to-32", "-O3", "-DNDEBUG"]),
-                .define("GGML_USE_ACCELERATE"),
                 .unsafeFlags(["-fno-objc-arc"]),
-                .define("GGML_USE_METAL")
-                // NOTE: NEW_LAPACK will required iOS version 16.4+
-                // We should consider add this in the future when we drop support for iOS 14
-                // (ref: ref: https://developer.apple.com/documentation/accelerate/1513264-cblas_sgemm?language=objc)
+                .define("GGML_USE_ACCELERATE"),
+                .define("WHISPER_USE_COREML")
                 // .define("ACCELERATE_NEW_LAPACK"),
                 // .define("ACCELERATE_LAPACK_ILP64")
             ],
